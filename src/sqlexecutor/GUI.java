cf29a7e2dad65695a21edd9b438adfe70bb780ed
SergeyLebidko <it.sergeyler@mail.ru>
2019-04-26 15:13:58 +0300

    Обработчики событий для кнопок перенесены в отдельные методы

--- src/sqlexecutor/GUI.java
+++ src/sqlexecutor/GUI.java
@@ -56,7 +56,7 @@ public class GUI {
             executeQueryBtn = new JButton("Выполнить запрос");
             sqlQueryArea.setBorder(BorderFactory.createEtchedBorder(EtchedBorder.LOWERED));
             JPanel topPane = new JPanel();
-            topPane.setLayout(new BorderLayout(5,5));
+            topPane.setLayout(new BorderLayout(5, 5));
             Box box1 = Box.createHorizontalBox();
             box1.add(sqlQueryArea);
             box1.add(Box.createHorizontalStrut(5));
@@ -91,62 +91,7 @@ public class GUI {
             contentPane.add(mainTable.getContentPane(), BorderLayout.CENTER);
         }
 
-        executeQueryBtn.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                mainTable.executeQuery(sqlQueryArea.getText());
-            }
-        });
-
-        ActionListener showBtnListener = new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                JDialog dialog = new JDialog(frm, false);
-                String title="";
-                String query="";
-                switch (((JButton)e.getSource()).getText()){
-                    case "Покупатели":{
-                        title="Таблица Покупатели (CUSTOMERS)";
-                        query="SELECT * FROM CUSTOMERS";
-                        break;
-                    }
-                    case "Офисы":{
-                        title="Таблица Офисы (OFFICES)";
-                        query="SELECT * FROM OFFICES";
-                        break;
-                    }
-                    case "Заказы":{
-                        title="Таблица Заказы (ORDERS)";
-                        query="SELECT * FROM ORDERS";
-                        break;
-                    }
-                    case "Товары":{
-                        title="Таблица Товары (PRODUCTS)";
-                        query="SELECT * FROM PRODUCTS";
-                        break;
-                    }
-                    case "Служащие":{
-                        title="Таблица Служащие (SALESREPS)";
-                        query="SELECT * FROM SALESREPS";
-                    }
-                }
-
-                dialog.setTitle(title);
-                dialog.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
-                dialog.setSize(new Dimension(WIDTH_DIALOG, HEIGHT_DIALOG));
-
-                JPanel dialogPane = new JPanel();
-                dialogPane.setLayout(new BorderLayout(5,5));
-                dialogPane.setBorder(BorderFactory.createEmptyBorder(5,5,5,5));
-
-                ResultTable dialogTable = new ResultTable(dataBaseConnector);
-                dialogTable.executeQuery(query);
-                dialogPane.add(dialogTable.getContentPane(), BorderLayout.CENTER);
-
-                dialog.add(dialogPane, BorderLayout.CENTER);
-                dialog.setVisible(true);
-            }
-        };
+        executeQueryBtn.addActionListener(executeQueryBtnListener);
 
         showCustomers.addActionListener(showBtnListener);
         showOffices.addActionListener(showBtnListener);
@@ -157,5 +102,62 @@ public class GUI {
         frm.setContentPane(contentPane);
         frm.setVisible(true);
     }
+
+    private ActionListener executeQueryBtnListener = new ActionListener() {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            mainTable.executeQuery(sqlQueryArea.getText());
+        }
+    };
+
+    private ActionListener showBtnListener = new ActionListener() {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            JDialog dialog = new JDialog(frm, false);
+            String title = "";
+            String query = "";
+            switch (((JButton) e.getSource()).getText()) {
+                case "Покупатели": {
+                    title = "Таблица Покупатели (CUSTOMERS)";
+                    query = "SELECT * FROM CUSTOMERS";
+                    break;
+                }
+                case "Офисы": {
+                    title = "Таблица Офисы (OFFICES)";
+                    query = "SELECT * FROM OFFICES";
+                    break;
+                }
+                case "Заказы": {
+                    title = "Таблица Заказы (ORDERS)";
+                    query = "SELECT * FROM ORDERS";
+                    break;
+                }
+                case "Товары": {
+                    title = "Таблица Товары (PRODUCTS)";
+                    query = "SELECT * FROM PRODUCTS";
+                    break;
+                }
+                case "Служащие": {
+                    title = "Таблица Служащие (SALESREPS)";
+                    query = "SELECT * FROM SALESREPS";
+                }
+            }
+
+            dialog.setTitle(title);
+            dialog.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
+            dialog.setSize(new Dimension(WIDTH_DIALOG, HEIGHT_DIALOG));
+
+            JPanel dialogPane = new JPanel();
+            dialogPane.setLayout(new BorderLayout(5, 5));
+            dialogPane.setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5));
+
+            ResultTable dialogTable = new ResultTable(dataBaseConnector);
+            dialogTable.executeQuery(query);
+            dialogPane.add(dialogTable.getContentPane(), BorderLayout.CENTER);
+
+            dialog.add(dialogPane, BorderLayout.CENTER);
+            dialog.setVisible(true);
+        }
+    };
 
 }
